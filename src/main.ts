7a8072101ec94eca422178c79a265199c399229c
marcosgrm <marcos.grm@yahoo.com>
2019-10-23 11:41:33 -0300

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+let decimal: number = 10;
+let hexadecimal: number = 0xf10b;
+let binario: number = 0b110100;
+let octal: number = 0o410;
+
+console.log(decimal);
+console.log(hexadecimal);
+console.log(binario);
+console.log(octal);
+
+let a: number = 634333333333333333336;
+
+console.log(a.toPrecision(5));
