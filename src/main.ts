193aa86309e08bd190cb4e24c3fb61aa2f77c8da
marcosgrm <marcos.grm@yahoo.com>
2019-10-29 21:27:57 -0200

    Último slide: arrow functions

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-let decimal: number = 10;
-let hexadecimal: number = 0xf10b;
-let binario: number = 0b110100;
-let octal: number = 0o410;
+let sum = (x: number, y: number): number => {
+    return x + y;
+}
 
-console.log(decimal);
-console.log(hexadecimal);
-console.log(binario);
-console.log(octal);
+let soma = sum(2,3);
 
-let a: number = 634333333333333333336;
-
-console.log(a.toPrecision(5));
+console.log(soma);
